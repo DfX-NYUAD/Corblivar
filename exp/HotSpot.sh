44fab036363128f4b3815e7fd160710d04397cd7
Johann Knechtel <johann.knechtel@ifte.de>
2013-02-26 16:25:12 +0100

    update exp scripts; relaxed config parameters for n100

--- exp/HotSpot.sh
+++ exp/HotSpot.sh
@@ -12,8 +12,9 @@ PTRACE=$1_HotSpot.ptrace
 GRID_LCF=$1_HotSpot.lcf
 STEADY_OUTPUT=$1_HotSpot.steady
 STEADY_GRID_OUTPUT=$1_HotSpot.steady.grid
+DIM=64
 
 # perform HS call
-$HS/hotspot -c $HS/hotspot.config -f $BOTTOM_FP -p $PTRACE -grid_steady_file $STEADY_GRID_OUTPUT -steady_file $STEADY_OUTPUT -model_type grid -grid_map_mode avg -detailed_3D on -grid_layer_file $GRID_LCF
+$HS/hotspot -c $HS/hotspot.config -f $BOTTOM_FP -p $PTRACE -grid_steady_file $STEADY_GRID_OUTPUT -steady_file $STEADY_OUTPUT -model_type grid -grid_map_mode max -detailed_3D on -grid_layer_file $GRID_LCF -grid_rows $DIM -grid_cols $DIM
 # render SVG of temperature map
-$HS/grid_thermal_map.pl $BOTTOM_FP $STEADY_GRID_OUTPUT > $BOTTOM_FP.svg
+$HS/grid_thermal_map.pl $BOTTOM_FP $STEADY_GRID_OUTPUT $DIM $DIM > $BOTTOM_FP.svg
--- exp/clean.sh
+++ exp/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for type in gp eps solution flp ptrace lcf steady grid svg
+for type in gp eps solution flp ptrace lcf steady grid svg data
 do
 	for file in `ls *.$type`
 	do
