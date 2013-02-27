6b614d23abb4d060b384316ec447b0f6fd1b7301
Johann Knechtel <johann.knechtel@ifte.de>
2013-02-27 14:34:24 +0100

    update release scripts

--- Release.sh
+++ Release.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 REV=commit.git
-ARCHIVE=Corblivar.tar.gz
+ARCHIVE=Corblivar.zip
 SERVER=ganymed
 
 # annotate current git commit
 git show --pretty=oneline | head -n 1 > $REV
 
 # create archive
-tar -czf $ARCHIVE $REV src/*.cpp src/*.hpp Makefile exp/*.sh exp/*.conf
+zip -r $ARCHIVE $REV src/*.cpp src/*.hpp Makefile exp/*.sh exp/*.conf exp/bench/n*
 
 # push archive
 scp $ARCHIVE $SERVER:public_html/
+
+# clean stuff
+rm $REV $ARCHIVE
