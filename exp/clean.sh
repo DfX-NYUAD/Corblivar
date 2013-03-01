fd8dbf036b4f64276a60549d19f8b27056c6e5af
Johann Knechtel <johann.knechtel@ifte.de>
2013-03-01 11:16:53 +0100

    minor update exp script

--- exp/clean.sh
+++ exp/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for type in gp eps solution flp ptrace lcf steady grid svg data
+for type in gp eps solution flp ptrace lcf steady grid svg data results
 do
 	for file in `ls *.$type`
 	do
