f9a1f350a5b5a001c2e36b75da2ea3e6fbaaf83a
Johann Knechtel <johann.knechtel@ifte.de>
2013-04-18 19:26:44 +0200

    minor update exp script, dump HotSpot log into file

--- exp/HotSpot.sh
+++ exp/HotSpot.sh
@@ -13,9 +13,10 @@ GRID_LCF=$1_HotSpot.lcf
 STEADY_OUTPUT=$1_HotSpot.steady
 STEADY_GRID_OUTPUT=$1_HotSpot.steady.grid
 DIM=64
+LOG=$1_HotSpot.log
 
 # perform HS call
-$HS/hotspot -c $HS/hotspot.config -f $BOTTOM_FP -p $PTRACE -grid_steady_file $STEADY_GRID_OUTPUT -steady_file $STEADY_OUTPUT -model_type grid -grid_map_mode max -detailed_3D on -grid_layer_file $GRID_LCF -grid_rows $DIM -grid_cols $DIM
+$HS/hotspot -c $HS/hotspot.config -f $BOTTOM_FP -p $PTRACE -grid_steady_file $STEADY_GRID_OUTPUT -steady_file $STEADY_OUTPUT -model_type grid -grid_map_mode max -detailed_3D on -grid_layer_file $GRID_LCF -grid_rows $DIM -grid_cols $DIM > $LOG
 # render SVG of temperature map
 $HS/grid_thermal_map.pl $BOTTOM_FP $STEADY_GRID_OUTPUT $DIM $DIM > $BOTTOM_FP.svg
 # generate PDF
