71265aa1beec9f607eec21ef7bd3165c3376e479
Johann Knechtel <johann.knechtel@ifte.de>
2013-05-14 12:16:31 +0200

    minor update comments for power-aware block handling

--- src/IO.cpp
+++ src/IO.cpp
@@ -375,11 +375,11 @@ void IO::parseParameterConfig(FloorPlanner& fp, int const& argc, char** argv) {
 		cout << "IO>  SA -- Layout generation; guided hard block rotation: " << fp.conf_SA_layout_enhanced_hard_block_rotation << endl;
 		cout << "IO>  SA -- Layout generation; guided soft block shaping: " << fp.conf_SA_layout_enhanced_soft_block_shaping << endl;
 		cout << "IO>  SA -- Layout generation; packing iterations: " << fp.conf_SA_layout_packing_iterations << endl;
-		cout << "IO>  SA -- Layout generation; power-guided block handling: " << fp.conf_SA_layout_power_aware_block_handling << endl;
-		// consider power-guided block handling only if thermal optimization is on
+		cout << "IO>  SA -- Layout generation; power-aware block handling: " << fp.conf_SA_layout_power_aware_block_handling << endl;
+		// consider power-aware block handling only if thermal optimization is on
 		if (!fp.conf_SA_opt_thermal) {
 			fp.conf_SA_layout_power_aware_block_handling = false;
-			cout << "IO>     Note: power-guided block handling is ignored since thermal optimization is disabled" << endl;
+			cout << "IO>     Note: power-aware block handling is ignored since thermal optimization is disabled" << endl;
 		}
 		cout << "IO>  SA -- Layout generation; floorplacement handling: " << fp.conf_SA_layout_floorplacement << endl;
 
