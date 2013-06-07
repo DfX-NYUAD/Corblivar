1bab5f94eff484f8b573afd5a70b25f6b1a29c05
Johann Knechtel <johann.knechtel@ifte.de>
2013-06-07 10:21:02 +0200

    updates class CorblivarAlignmentReq; update logging, update notation

--- src/CorblivarAlignmentReq.hpp
+++ src/CorblivarAlignmentReq.hpp
@@ -27,14 +27,14 @@ class CorblivarAlignmentReq {
 
 	// private data, functions
 	private:
-		CorblivarAlignmentReq::Type type_x;
-		CorblivarAlignmentReq::Type type_y;
+		Type type_x;
+		Type type_y;
 
 	// constructors, destructors, if any non-implicit
 	public:
 		CorblivarAlignmentReq(int const& id, Block const* s_i, Block const* s_j,
-				CorblivarAlignmentReq::Type const& type_x, double const& offset_range_x,
-				CorblivarAlignmentReq::Type const& type_y, double const& offset_range_y) {
+				Type const& type_x, double const& offset_range_x,
+				Type const& type_y, double const& offset_range_y) {
 			this->id = id;
 			this->s_i = s_i;
 			this->s_j = s_j;
@@ -62,28 +62,44 @@ class CorblivarAlignmentReq {
 		Block const* s_j;
 		double offset_range_x, offset_range_y;
 
-		friend ostream& operator<< (ostream& out, CorblivarAlignmentReq::Type const& type) {
-			out << static_cast<int>(type);
+		friend ostream& operator<< (ostream& out, Type const& type) {
+
+			switch (type) {
+
+				case Type::RANGE:
+					out << "RANGE";
+					break;
+				case Type::RANGE_MAX:
+					out << "RANGE_MAX";
+					break;
+				case Type::OFFSET:
+					out << "OFFSET";
+					break;
+				default:
+					out << "UNDEF";
+					break;
+			}
+
 			return out;
 		}
 
 		inline bool range_x() const {
-			return (this->type_x == CorblivarAlignmentReq::Type::RANGE && this->offset_range_x != 0.0);
+			return (this->type_x == Type::RANGE && this->offset_range_x != 0.0);
 		}
 		inline bool range_y() const {
-			return (this->type_y == CorblivarAlignmentReq::Type::RANGE && this->offset_range_y != 0.0);
+			return (this->type_y == Type::RANGE && this->offset_range_y != 0.0);
 		}
 		inline bool range_max_x() const {
-			return (this->type_x == CorblivarAlignmentReq::Type::RANGE_MAX && this->offset_range_x != 0.0);
+			return (this->type_x == Type::RANGE_MAX && this->offset_range_x != 0.0);
 		}
 		inline bool range_max_y() const {
-			return (this->type_y == CorblivarAlignmentReq::Type::RANGE_MAX && this->offset_range_y != 0.0);
+			return (this->type_y == Type::RANGE_MAX && this->offset_range_y != 0.0);
 		}
 		inline bool offset_x() const {
-			return (this->type_x == CorblivarAlignmentReq::Type::OFFSET);
+			return (this->type_x == Type::OFFSET);
 		}
 		inline bool offset_y() const {
-			return (this->type_y == CorblivarAlignmentReq::Type::OFFSET);
+			return (this->type_y == Type::OFFSET);
 		}
 
 		inline string tupleString() const {
