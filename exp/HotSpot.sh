44f573ed8f31654e6c07d6cc391cf528233f30bf
Johann Knechtel <johann.knechtel@ifte.de>
2013-08-21 16:19:38 +0200

    minor update config file, hotspot script

--- exp/HotSpot.sh
+++ exp/HotSpot.sh
@@ -17,11 +17,7 @@ PTRACE=$1_HotSpot.ptrace
 GRID_LCF=$1_HotSpot.lcf
 STEADY_OUTPUT=$1_HotSpot.steady
 STEADY_GRID_OUTPUT=$1_HotSpot.steady.grid
-GRID_LCF_TSVS=$1_HotSpot_TSVs.lcf
-STEADY_OUTPUT_TSVS=$1_HotSpot_TSVs.steady
-STEADY_GRID_OUTPUT_TSVS=$1_HotSpot_TSVs.steady.grid
 LOG=$1_HotSpot.txt
-LOG_TSVS=$1_HotSpot_TSVs.txt
 DIM=64
 
 # perform HS call
@@ -29,13 +25,6 @@ echo "Perform HotSpot run ..."
 $HS/hotspot -c $HS/hotspot.config -f $DUMMY_FP -p $PTRACE -grid_steady_file $STEADY_GRID_OUTPUT -steady_file $STEADY_OUTPUT -model_type grid -grid_map_mode max -detailed_3D on -grid_layer_file $GRID_LCF -grid_rows $DIM -grid_cols $DIM > $LOG
 # log hottest block
 tail -n1 $LOG
-
-if [ "$3" == "1" ]; then
-	echo "Perform HotSpot w/ TSVs run ..."
-	$HS/hotspot -c $HS/hotspot.config -f $DUMMY_FP -p $PTRACE -grid_steady_file $STEADY_GRID_OUTPUT_TSVS -steady_file $STEADY_OUTPUT_TSVS -model_type grid -grid_map_mode max -detailed_3D on -grid_layer_file $GRID_LCF_TSVS -grid_rows $DIM -grid_cols $DIM > $LOG_TSVS
-	# log hottest block
-	tail -n1 $LOG
-fi
 
 # render temperature map for active layers; layer IDs must correspond to activ Si layers
 # defined in  GRID_LCF
@@ -56,23 +45,3 @@ do
 	# consider next die
 	DIE=$(($DIE + 1))
 done
-
-if [ "$3" == "1" ]; then
-	DIE=1
-	for (( layer = 1; layer < ($2 * 4); layer = layer + 4 ))
-	do
-		echo "Generate temperature map w/ TSVs for die $DIE"
-
-		FP=$1_HotSpot_$DIE.flp
-
-		# render SVG
-		$HS/grid_thermal_map.pl $FP $STEADY_GRID_OUTPUT_TSVS.layer_$layer $DIM $DIM > $FP"_TSVs.svg"
-		# generate PDF
-		cairosvg-py3 $FP"_TSVs.svg" -f pdf -o $FP"_TSVs.pdf"
-		# drop svg
-		rm $FP"_TSVs.svg"
-
-		# consider next die
-		DIE=$(($DIE + 1))
-	done
-fi
