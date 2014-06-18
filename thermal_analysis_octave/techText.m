563719eb375c8681a2fea4eaf5364f86f7bb74ba
Johann Knechtel <johann.knechtel@ifte.de>
2014-06-18 15:07:09 +0200

    update/fix octave scripts; parsing of layer count from new Technology.conf

--- thermal_analysis_octave/techText.m
+++ thermal_analysis_octave/techText.m
@@ -1,8 +1,8 @@
-function [conf, p]= confText(conf, p)
+function [conf, p]= techText(conf, p)
 
-%% Description:  Integrated Octave function for reading the "Corblivar.conf" file and creating a string array confText for further internal use
+%% Description:  Integrated Octave function for reading the "Technology.conf" file and parsing the layer count
 
-%% Copyright (C) 2013 Timm Amstein,timm.amstein@gmail.com, Johann Knechtel, johann.knechtel@ifte.de, www.ifte.
+%% Copyright (C) 2013 Johann Knechtel, johann.knechtel@ifte.de, www.ifte.
 
 %% This file is part of Corblivar.
 
@@ -20,12 +20,18 @@ function [conf, p]= confText(conf, p)
 %%    Public License along with Corblivar.  If not, see <http://www.gnu.org/licenses/>.
 
 
-conf.text = fopen(conf.path,'rt');	% Octave opens Text temporarily in own format
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% read layers from technology file
 
-%% initiate line counter and get read first line of Configtext
+path_tech = sprintf('%s%s', conf.dir, p.opt.technology.value);
+path_tech
+%path_tech2 = sprintf('%sTechnology.conf', conf.dir);
+%path_tech2
+
+technology = fopen(path_tech,'rt');	% Octave opens Text temporarily in own format
 
 line = 1;
-text = fgetl(conf.text);	% fgetl puts out first line of text and erases the same in the original
+text = fgetl(technology);	% fgetl puts out first line of text and erases the same in the original
 
 %% initialize variables for distinction of cases and check-string to import the layers parameter  
 
@@ -37,17 +43,20 @@ k = 0;
 while k < 1
 
 	line++;							% line counter
-	temp = fgetl (conf.text);				% get next unread line of text 
+	temp = fgetl (technology);				% get next unread line of text 
 		
-	if 	strncmp(p.opt.technology.check, temp,12)	% search for technology file string
+	if 	strncmp(p.opt.layers.check, temp,8)	% search for checkLayers string
 
 		h = line;					% safe line (there is the definition and the name of parameter in config file)
 	endif	
 
-	if line == (h+2)					% get line two lines below the safed one (there is the file string parameter)
+	if line == (h+2)					% get line two lines below the safed one (there is the numerical parameter)
 
-		file = temp;
-		p.opt.technology.value = strtrim(file);		% extract file parameter, trim whitespaces
+		layers = temp;					% extract layers parameter
+		p.opt.layers.value = str2num(layers);	% convert extracted string into number
+
+		k = 1;
+		break
 	endif
 
 	if temp == -1						% no more lines to read --> stop while-loop
@@ -57,16 +66,4 @@ while k < 1
 		break
 	endif
 
-	text = [text ; temp];					% update the string array for confText and safe the temporary line beneath the last safed one
-
 endwhile
-
-%% save temporary string array as confText
-
-conf.text = text;
-
-%% get size of the string array
-
-conf.sct = size(conf.text);
-
-end
