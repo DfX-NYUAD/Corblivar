41917c982eb32bb5006ab1cc17cbd125b7f3a1c4
Johann Knechtel <jknechtel@masdar.ac.ae>
2015-04-22 17:59:40 +0400

    update CorblivarAlignmentReq::vertical_bus, consider also non-zero offset with sufficient resulting overlap b/w blocks

--- src/CorblivarAlignmentReq.hpp
+++ src/CorblivarAlignmentReq.hpp
@@ -160,10 +160,21 @@ class CorblivarAlignmentReq {
 		//
 		inline bool vertical_bus() const {
 			return (
-				// zero-offset fixed alignment in both coordinates
-				(this->offset_x() && this->alignment_x == 0 && this->offset_y() && this->alignment_y == 0) ||
 				// min overlap in both dimensions
-				(this->range_x() && this->range_y())
+				(this->range_x() && this->range_y()) ||
+				// zero-offset fixed alignment in both dimensions
+				(this->offset_x() && this->alignment_x == 0 && this->offset_y() && this->alignment_y == 0) ||
+				// non-zero offset in both dimensions, but with
+				// sufficiently small offset such that blocks will
+				// partially intersect
+				(this->offset_x() && this->offset_y() &&
+					// positive offset: b_j not further offset to the
+					// right/top than b_j is wide/high; negative
+					// offset: b_j not further offset to the
+					// left/bottom than b_j is wide/high
+				 	this->alignment_x > 0.0 ? this->alignment_x < this->s_i->bb.w : this->alignment_x > -(this->s_j->bb.w) &&
+				 	this->alignment_y > 0.0 ? this->alignment_y < this->s_i->bb.h : this->alignment_y > -(this->s_j->bb.h)
+				)
 			);
 		}
 		inline bool partner_blocks(Block const* b1, Block const* b2) const {
