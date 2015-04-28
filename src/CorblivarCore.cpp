8ca8834443e0822c12f7ed7693dfd20261641154
Johann Knechtel <jknechtel@masdar.ac.ae>
2015-04-28 13:22:31 +0400

    Revert "fix CorblivarCore::generateLayout, use references to this->p instead of"
    This reverts commit d4332de0b242271f660c95d69fd499a4b3ed526a.
    
    Block alignment was buggy after this and the previous commit.

--- src/CorblivarCore.cpp
+++ src/CorblivarCore.cpp
@@ -288,7 +288,7 @@ bool CorblivarCore::generateLayout(bool const& perform_alignment) {
 						if (req_processed == nullptr) {
 
 							// stall layout generation on this die;
-							this->p->stalled = true;
+							this->dies[cur_block->layer].stalled = true;
 							// memorize alignment as in process;
 							this->AL.push_back(cur_req);
 							// continue layout generation on die
@@ -308,21 +308,21 @@ bool CorblivarCore::generateLayout(bool const& perform_alignment) {
 						}
 					}
 
-					// if this die is not stalled after handling
-					// requests, then all requests were handled
-					// successfully and further layout generation to
-					// be continued on this die;
+					// check if any request at all was processed
 					//
-					// that means, increment progress pointer since
-					// block and related alignment requests are
-					// handled
-					if (!this->p->stalled) {
+					if (alignments_processed) {
 
-						if (CorblivarAlignmentReq::DBG_LAYOUT_GENERATION) {
-							std::cout << "DBG_ALIGNMENT>  All requests handled for block " << cur_block->id << "; continue w/ next block" << std::endl;
+						// all requests are handled and further layout generation
+						// to be continued on this die; increment progress pointer
+						// since block and related alignment requests are handled
+						if (!this->dies[cur_block->layer].stalled) {
+
+							if (CorblivarAlignmentReq::DBG_LAYOUT_GENERATION) {
+								std::cout << "DBG_ALIGNMENT>  All requests handled for block " << cur_block->id << "; continue w/ next block" << std::endl;
+							}
+
+							this->p->updateProgressPointerFlag();
 						}
-
-						this->p->updateProgressPointerFlag();
 					}
 				}
 
